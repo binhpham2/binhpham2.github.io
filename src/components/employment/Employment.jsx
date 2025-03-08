220e209f22974b4c72254894a3a908569c399ff1
Binh Pham <binh.pham5918@gmail.com>
2025-03-08 16:26:42 +0200

    component structure comments

--- src/components/employment/Employment.jsx
+++ src/components/employment/Employment.jsx
@@ -3,6 +3,19 @@ import EmploymentYoESummary from './EmploymentYoESummary'
 import EmploymentTechSummary from './EmploymentTechSummary'
 import EmploymentHistory from './EmploymentHistory'
 
+/*--------------------------------
+Component structure:
+
+Employment
+    h1 (text "Employment")
+    div (class EmploymentContent)
+        div (class EmploymentSummary)
+            EmploymentYoESummary
+            EmploymentTechSummary
+        EmploymentHistory
+            EmploymentHistoryCard
+--------------------------------*/
+
 const Employment = () => {
     return (
         <div className='Employment'>
