8883857b6070146a1847bc4ade0a9484837a3d1c
Binh Pham <binh.pham5918@gmail.com>
2025-03-23 01:02:48 +0200

    employment rework, panel & app minor update in css rules

--- src/components/employment/Employment.jsx
+++ src/components/employment/Employment.jsx
@@ -1,6 +1,4 @@
 import './employment.css'
-import EmploymentYoESummary from './EmploymentYoESummary'
-import EmploymentTechSummary from './EmploymentTechSummary'
 import EmploymentHistory from './EmploymentHistory'
 
 /*--------------------------------
@@ -8,25 +6,15 @@ Component structure:
 
 Employment
     h1 (text "Employment")
-    div (class EmploymentContent)
-        div (class EmploymentSummary)
-            EmploymentYoESummary
-            EmploymentTechSummary
-        EmploymentHistory
-            EmploymentHistoryCard
+    EmploymentHistory
+        EmploymentHistoryCard
 --------------------------------*/
 
 const Employment = () => {
     return (
         <div className='Employment'>
             <h1>Employment</h1>
-            <div className='EmploymentContent'>
-                <div className='EmploymentSummary'>
-                    <EmploymentYoESummary />
-                    <EmploymentTechSummary />
-                </div>
-                <EmploymentHistory />
-            </div>
+            <EmploymentHistory />
         </div>
     )
 }
--- src/components/employment/EmploymentHistoryCard.jsx
+++ src/components/employment/EmploymentHistoryCard.jsx
@@ -3,17 +3,8 @@ import './employment.css'
 const EmploymentHistoryCard = ({ employment }) => {
     return (
         <div className='EmploymentHistoryCard'>
-            
-            {/* Front face of the card */}
-            <div>
-                <p>{employment.company}</p>
-                <p>{employment.time}</p>
-            </div>
-            {/* Back face of the card */}
-            <div>
-                haha
-                
-            </div>
+            <p>{employment.company}</p>
+            <p>{employment.time}</p>
         </div>
     )
 }
