892115ac939c7be26bec9197832c4b44edbc0ce1
Binh Pham <binh.pham5918@gmail.com>
2025-03-24 02:09:13 +0200

    employment section -wip

---
+++ src/data/employment.js
@@ -0,0 +1,66 @@
+export const employments = [
+    {
+        'company': 'KONE',
+        'time': 'May 2024 - May 2025',
+        'positions': [
+            {
+                'title': 'Master Thesis Worker',
+                'time': 'December 2024 - May 2025',
+            },
+            {
+                'title': 'Data Engineer Intern',
+                'time': 'May 2024 - December 2024',
+            },
+        ]
+    },
+    {
+        'company': 'ABB',
+        'time': 'Jan 2023 - Aug 2023',
+        'positions': [
+            {
+                'title': 'Data Analyst Intern',
+                'time': 'Jan 2023 - Aug 2023',
+            }
+        ]
+    },
+    {
+        'company': 'Elisa',
+        'time': 'May 2022 - Aug 2022',
+        'positions': [
+            {
+                'title': 'Software Engineer Intern',
+                'time': 'May 2022 - Aug 2022',
+            }
+        ]
+    },
+    {
+        'company': 'Futurice',
+        'time': 'Jan 2022 - Apr 2022',
+        'positions': [
+            {
+                'title': 'Software Engineer Intern',
+                'time': 'May 2022 - Aug 2022',
+            }
+        ]
+    },
+    {
+        'company': 'Aalto University',
+        'time': 'Apr 2021 - Jun 2021',
+        'positions': [
+            {
+                'title': 'Teaching Assistant',
+                'time': 'Apr 2021 - Jun 2021',
+            }
+        ]
+    },
+    {
+        'company': 'Upwork',
+        'time': 'Feb 2021 - Mar 2021',
+        'positions': [
+            {
+                'title': 'Game Developer',
+                'time': 'Feb 2021 - Mar 2021',
+            }
+        ]
+    },
+]
