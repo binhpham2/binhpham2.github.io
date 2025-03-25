831d78db597980284147b004224c09fc5a7d83d7
Binh Pham <binh.pham5918@gmail.com>
2025-03-25 12:21:45 +0200

    employment section p1

--- src/components/employment/EmploymentHistoryCard.jsx
+++ src/components/employment/EmploymentHistoryCard.jsx
@@ -1,10 +1,29 @@
 import './employment.css'
+import koneLogo from '../../images/logos/kone.png'
+import abbLogo from '../../images/logos/abb.png'
+import elisaLogo from '../../images/logos/elisa.png'
+import futuriceLogo from '../../images/logos/futurice.png'
+import aaltoLogo from '../../images/logos/aalto.png'
+import upworkLogo from '../../images/logos/upwork.png'
 
 const EmploymentHistoryCard = ({ employment }) => {
+
+    const companyLogos = {
+        'KONE': koneLogo,
+        'ABB': abbLogo,
+        'Elisa': elisaLogo,
+        'Futurice': futuriceLogo,
+        'Aalto University': aaltoLogo,
+        'Upwork': upworkLogo
+    }
+
     return (
         <div className='EmploymentHistoryCard'>
-            <p>{employment.company}</p>
-            <p>{employment.time}</p>
+            <img 
+                src={companyLogos[employment.company]} 
+                alt={employment.company} 
+                className='EmploymentCompanyLogo' 
+            />
         </div>
     )
 }
