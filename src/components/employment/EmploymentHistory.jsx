6e56a9ce479f2bac40121bbbee5a2cf754da2ed0
Binh Pham <binh.pham5918@gmail.com>
2025-03-29 02:33:43 +0200

    add comments on employment section

--- src/components/employment/EmploymentHistory.jsx
+++ src/components/employment/EmploymentHistory.jsx
@@ -3,6 +3,11 @@ import EmploymentHistoryCard from './EmploymentHistoryCard'
 import { employments } from '../../data/employment'
 
 const EmploymentHistory = () => {
+    /* There are two type of employment cards. Let them be first stack and second stack. 
+    The reason there are those two stacks is to make the company logos scroll infinitely.
+    The first stack (type) is the one positioned on the left, and the second is on the right. 
+    The container (history) is displayed in flex, and aligns its items to flex-start. 
+    Therefore, the first type is visible in the beginning, and the second is visible on scrolling. */
     return (
         <div className='EmploymentHistory'>
             {employments.map(
