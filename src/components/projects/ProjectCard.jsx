c644ee4a32a1159a54928fc95a4f40470a2040bb
Binh Pham <binh.pham5918@gmail.com>
2025-04-04 20:35:28 +0300

    small style change in employment and project buttons

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -38,7 +38,7 @@ const ProjectCard = ({ project, cardHovered, setCardHovered }) => {
                 <p><span>Description:</span> {project.description}</p>
                 { project.note ? <p><span>Note:</span> {project.note}</p> : null }
                 <p><span>Stack:</span> {project.stack}</p>
-                <p><span>Source code:</span> <a href={project.link} target="_blank" rel="noopener noreferrer">View</a></p>
+                <p><a href={project.link} target="_blank" rel="noopener noreferrer">View code</a></p>
             </div>
         </div>
     )
